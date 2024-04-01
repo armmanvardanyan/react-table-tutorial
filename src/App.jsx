9dbebfdbb84671bc6f55671f8aa0d96282d0ff49
Arman <43242508+armmanvardanyan@users.noreply.github.com>
2024-04-01 22:50:30 +0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Box, Heading } from "@chakra-ui/react";
 import TaskTable from "./components/TaskTable";
 
 function App() {
+
+  
   return (
     <Box maxW={1000} mx="auto" px={6} pt={24} fontSize="sm">
       <Heading mb={10}>TanStack Table</Heading>
--- src/components/TaskTable.jsx
+++ src/components/TaskTable.jsx
@@ -1,151 +1,70 @@
-import { useState } from "react";
-import { Box, Button, ButtonGroup, Icon, Text } from "@chakra-ui/react";
-import {
-  flexRender,
-  getCoreRowModel,
-  getFilteredRowModel,
-  getPaginationRowModel,
-  getSortedRowModel,
-  useReactTable,
-} from "@tanstack/react-table";
+import { useReactTable, getCoreRowModel } from "@tanstack/react-table";
+import React from "react";
 import DATA from "../data";
-import EditableCell from "./EditableCell";
-import StatusCell from "./StatusCell";
-import DateCell from "./DateCell";
-import Filters from "./Filters";
-import SortIcon from "./icons/SortIcon";
+import { useState } from "react";
+import { Box } from "@chakra-ui/react";
+import { flexRender } from "@tanstack/react-table";
 
 const columns = [
   {
-    accessorKey: "task",
     header: "Task",
-    size: 225,
-    cell: EditableCell,
-    enableColumnFilter: true,
-    filterFn: "includesString",
+    accessorKey: "task",
+    cell: (props) => <p>{props.getValue()}</p>,
   },
   {
-    accessorKey: "status",
     header: "Status",
-    cell: StatusCell,
-    enableSorting: false,
-    enableColumnFilter: true,
-    filterFn: (row, columnId, filterStatuses) => {
-      if (filterStatuses.length === 0) return true;
-      const status = row.getValue(columnId);
-      return filterStatuses.includes(status?.id);
-    },
+    accessorKey: "status",
+    cell: (props) => <p>{props.getValue()?.name}</p>,
   },
   {
-    accessorKey: "due",
     header: "Due",
-    cell: DateCell,
+    accessorKey: "due",
+    cell: (props) => <p>{props.getValue()?.toLocaleTimeString()}</p>,
   },
   {
-    accessorKey: "notes",
     header: "Notes",
-    size: 225,
-    cell: EditableCell,
+    accessorKey: "notes",
+    cell: (props) => <p>{props.getValue()}</p>,
   },
 ];
 
-const TaskTable = () => {
+export default function TaskTable() {
   const [data, setData] = useState(DATA);
-  const [columnFilters, setColumnFilters] = useState([]);
 
   const table = useReactTable({
     data,
     columns,
-    state: {
-      columnFilters,
-    },
     getCoreRowModel: getCoreRowModel(),
-    getFilteredRowModel: getFilteredRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-    columnResizeMode: "onChange",
-    meta: {
-      updateData: (rowIndex, columnId, value) =>
-        setData((prev) =>
-          prev.map((row, index) =>
-            index === rowIndex
-              ? {
-                  ...prev[rowIndex],
-                  [columnId]: value,
-                }
-              : row
-          )
-        ),
-    },
   });
+
+  console.log(table.getRowModel());
 
   return (
     <Box>
-      <Filters
-        columnFilters={columnFilters}
-        setColumnFilters={setColumnFilters}
-      />
       <Box className="table" w={table.getTotalSize()}>
         {table.getHeaderGroups().map((headerGroup) => (
           <Box className="tr" key={headerGroup.id}>
             {headerGroup.headers.map((header) => (
-              <Box className="th" w={header.getSize()} key={header.id}>
+              <Box key={header.id} className="th" w={header.getSize()}>
                 {header.column.columnDef.header}
-                {header.column.getCanSort() && (
-                  <Icon
-                    as={SortIcon}
-                    mx={3}
-                    fontSize={14}
-                    onClick={header.column.getToggleSortingHandler()}
-                  />
-                )}
-                {
-                  {
-                    asc: " 🔼",
-                    desc: " 🔽",
-                  }[header.column.getIsSorted()]
-                }
-                <Box
-                  onMouseDown={header.getResizeHandler()}
-                  onTouchStart={header.getResizeHandler()}
-                  className={`resizer ${
-                    header.column.getIsResizing() ? "isResizing" : ""
-                  }`}
-                />
               </Box>
             ))}
           </Box>
         ))}
-        {table.getRowModel().rows.map((row) => (
-          <Box className="tr" key={row.id}>
-            {row.getVisibleCells().map((cell) => (
-              <Box className="td" w={cell.column.getSize()} key={cell.id}>
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-              </Box>
-            ))}
-          </Box>
-        ))}
+        {table.getRowModel().rows.map((row) => {
+          return (
+            <Box className="tr" key={row.id}>
+              {row.getVisibleCells().map((cell) => {
+                return (
+                  <Box key={cell.id} w={cell.column.getSize()} className="td">
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </Box>
+                );
+              })}
+            </Box>
+          );
+        })}
       </Box>
-      <br />
-      <Text mb={2}>
-        Page {table.getState().pagination.pageIndex + 1} of{" "}
-        {table.getPageCount()}
-      </Text>
-      <ButtonGroup size="sm" isAttached variant="outline">
-        <Button
-          onClick={() => table.previousPage()}
-          isDisabled={!table.getCanPreviousPage()}
-        >
-          {"<"}
-        </Button>
-        <Button
-          onClick={() => table.nextPage()}
-          isDisabled={!table.getCanNextPage()}
-        >
-          {">"}
-        </Button>
-      </ButtonGroup>
     </Box>
   );
-};
-export default TaskTable;
+}
